3c46fb8cd5373540fe3fe67dc35eefb9e38bd53f
Joanna Hsu <joannahsu@Joannas-MacBook-Pro.local>
2013-04-05 14:50:24 -0700

    april 5 changes, made it to final pages...

--- course_catalog_scrape.py
+++ course_catalog_scrape.py
@@ -1,9 +1,7 @@
 import urllib2
 import bs4
-
 address="http://general-catalog.berkeley.edu/gc/curricula.html"
 html = urllib2.urlopen(address)
-
 #get links to department pages
 soup = bs4.BeautifulSoup(html)
 soup.title ##cool!
@@ -15,11 +13,36 @@ for url in anchor_tags:
 			good_urls.append(url.attrs["href"])
 	except KeyError: #skips error
 		continue
-
+final_course_urls=[]
 for url in good_urls:
-	html = urllib2.urlopen(str(good_urls[0]))
+	course_urls=[]
+	html = urllib2.urlopen(str(url))
 	soup=bs4.BeautifulSoup(html, "html5lib")
-	print url
+	#course_links=soup.find_all("a")#[2]
+	print(url)
+	for link in soup.find_all("a"):
+		course_urls.append(link.get('href'))
+	course_urls.pop(0) #remove first one, always useless
+	for sub_url in course_urls:
+		if sub_url[:18]=="gcc_list_crse_req?": #filter out non-coruse catalog
+			final_course_urls.append(sub_url)
+			print (sub_url)
+final_course_urls=list(set(final_course_urls)) #drop duplicate department pages, down to 111
+for i, p in enumerate(final_course_urls): #loop to add "http://... in front of each web address"
+	final_course_urls[i]="http://general-catalog.berkeley.edu/catalog/"+p
+##FINALLY READY TO SCRAPE FROM THE PAGES OF INTEREST
+for sub_url in final_course_urls:
+	html = urllib2.urlopen(string(sub_url[0])
+	soup=bs4.BeautifulSoup(html, "html5lib")
+#for sub_url in course_links:
+	#try:
+		#if# sub_url.attrs["href"][:73]=="http://general-catalog.berkeley.edu/catalog/gcc_list_crse_req?p_dept_name":  #filter out non-course catalog links
+			#course_urls.append(url.attrs["href"])
+#sub_url in course_links:
+	#print(sub_url.get("href"))
+	#course_urls.append(url.attrs["href"])
+	#print url
+	#good_urls.
 	#soup.find_all('b')[2] #navigate the tree to grab the anchor tags below this b tag
 	# then store each one in another variable called, e.g., dept_sub_urls = []
 	
