2ae72b6b0157818f3eab055d2cc7c2f9832fcae5
Joanna Hsu <joannahsu@darpc.gateway.2wire.net>
2013-04-08 13:48:39 -0700

    4/8

--- course_catalog_scrape.py
+++ course_catalog_scrape.py
@@ -1,5 +1,7 @@
 import urllib2
 import bs4
+import re
+
 address="http://general-catalog.berkeley.edu/gc/curricula.html"
 html = urllib2.urlopen(address)
 #get links to department pages
@@ -30,10 +32,37 @@ for url in good_urls:
 final_course_urls=list(set(final_course_urls)) #drop duplicate department pages, down to 111
 for i, p in enumerate(final_course_urls): #loop to add "http://... in front of each web address"
 	final_course_urls[i]="http://general-catalog.berkeley.edu/catalog/"+p
-##FINALLY READY TO SCRAPE FROM THE PAGES OF INTEREST
+	##FINALLY READY TO SCRAPE FROM THE PAGES OF INTEREST
 for sub_url in final_course_urls:
 	html = urllib2.urlopen(string(sub_url[0])
 	soup=bs4.BeautifulSoup(html, "html5lib")
+	bold_tags=soup.find_all("b")
+	bold_tags.pop(0) #Drop first bold tag, which is not a course
+#BUILD A DICTIONARY TO HOLD THE DATA
+courses={}
+counter=0
+for iCourse in bold_tags:
+	#number=re.findall('\d+\.','<B>[1-9]. class.')
+	#m = re.match(r"\<b\>(\w+)\.(\s+)([\w\s]+)(.*)", "<b>11111AAA111111111111111111.  Military Physical Fitness and Nutrition. (1)  </b>")
+	m = re.match(r"\<b\>(?P<number>\w+)\.(\s+)(?P<title>[\w\s]+)(.*)",  "<b>11111AAA111111111111111111.  Military Physical Fitness and Nutrition. (1)  </b>")
+	m = re.match(r"\<b\>(?P<number>\w+)\.(\s+)(?P<title>[\w\s]+)(.*)",  iCourse.string.encode('ascii','ignore'))
+	m = re.match(r"\<b\>(?P<number>\w+)\.(\s+)(?P<title>[\w\s]+)(.*)",'1. Military Physical Fitness and Nutrition. (1) ')  
+	
+	#m = re.match(r"\<b\>(?P<number>\w+)\.(\s+)(?P<title>[\w\s]+)(.*)", "<b>11111AAA111111111111111111.  Military Physical Fitness and Nutrition. (1)  </b>")      
+	#print m.groups()
+	courses[str(counter)] = {}
+	courses[str(counter)] = m.groupdict()
+	counter += 1
+
+courses[str(counter)]['number'] = m.string[3]
+    #courses[str(counter)]['title'] = title
+    #courses[str(counter)]['dept_name_full'] = dept_name_full
+    #courses[str(counter)]['dept_code'] = dept_code
+    #[str(counter)]['desc'] = course_desc    
+    counter += 1
+
+	beautiful soup to get bold tags
+
 #for sub_url in course_links:
 	#try:
 		#if# sub_url.attrs["href"][:73]=="http://general-catalog.berkeley.edu/catalog/gcc_list_crse_req?p_dept_name":  #filter out non-course catalog links
@@ -53,4 +82,6 @@ for sub_url in final_course_urls:
 	#prettify
 
 	#grab in bold, to first period.  periods in paren
-	#end of course description (look for (F), (SP))
+	#end of course description (look for (F), (SP))#example regular expression to pull the course number. The first part is the expression and the second part will be whats in the tag..
+
+
--- runpython.py
+++ runpython.py
@@ -1,10 +1,10 @@
-course_urls=[]
-course_links=soup.find_all("a")#[2]
-for link in soup.find_all('a'):
-		course_urls.append(link.get('href'))
-course_urls.pop(0)
-final_course_urls=[]
-for sub_url in course_urls:
-	if sub_url[:30]=="gcc_list_crse_req?p_dept_name=": 
-		final_course_urls.append(sub_url)
-		
+courses={}
+counter=0
+for iCourse in bold_tags:
+	number=re.findall(, <b>124AB. Classname. )
+	courses[str(counter)] = {}
+	courses[str(counter)]['number'] = number
+	counter += 1
+
+m=re.match(r"\w+",str(bold_tags[4]))
+print m.groups()
